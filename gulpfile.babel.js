dd41c3064af98451aa15b280cc69e2f33acf1a55
Abdusa Alam <abdusa.alam@gmail.com>
2017-10-17 07:23:44 +0700

    Add sass task and several packages

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,7 @@ import gulp from 'gulp'
 import pug from 'gulp-pug'
 import plumber from 'gulp-plumber'
 import browserSync from 'browser-sync'
+import sass from 'gulp-sass'
 
 
 // Pug Compile
@@ -14,18 +15,28 @@ gulp.task('pug', () => {
         .pipe(gulp.dest('public'))
 })
 
+// SASS Compile
+gulp.task('sass', () => {
+  return gulp.src('./src/styles/styles.sass')
+    .pipe(plumber())
+    .pipe(sass({
+      precision: 4,
+      includePaths: './src/styles/',
+      outputStyle: 'expanded'
+    }))
+    .pipe(gulp.dest('./public/assets/css/'))
+})
+
 // browser Sync
 gulp.task('browserSync', () => {
-    browserSync.init({
-        server: {
-          baseDir: 'public',
-          index: 'index.html'
-        },
-        open: 'local',
-        notify: false
-      })
-
-    gulp.watch('src/views/index.pug', ['pug'], () => browserSync.reload())
+  browserSync.init({
+    server: {
+      baseDir: 'public',
+      index: 'index.html'
+    },
+    open: 'local',
+    notify: false
+  })
 })
 
 // Reloading browser if views changed

70fc129060e4330383174902ed838e69395b0c7f
Abdusa Alam <abdusa.alam@gmail.com>
2017-10-25 06:45:37 +0700

    Add automated copy to public for script files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,17 +2,18 @@ import gulp from 'gulp'
 import pug from 'gulp-pug'
 import plumber from 'gulp-plumber'
 import sass from 'gulp-sass'
+import changed from 'gulp-changed'
 const browserSync = require('browser-sync').create()
 
 
 // Pug Compile
 gulp.task('pug', () => {
-    return gulp.src('./src/views/index.pug')
-        .pipe(plumber())
-        .pipe(pug({
-            pretty: true
-        }))
-        .pipe(gulp.dest('public'))
+  return gulp.src('./src/views/index.pug')
+    .pipe(plumber())
+    .pipe(pug({
+      pretty: true
+    }))
+    .pipe(gulp.dest('public'))
 })
 
 // SASS Compile
@@ -37,7 +38,8 @@ gulp.task('browserSync', () => {
     },
     files: [
       'public/index.html',
-      'public/assets/css/styles.css'
+      'public/assets/css/styles.css',
+      'public/assets/js/main.js'
     ],
     open: 'local',
     notify: false,
@@ -45,11 +47,19 @@ gulp.task('browserSync', () => {
   })
 })
 
+// Handle vanilla javascript
+gulp.task('js', () => {
+  return gulp.src('src/scripts/main.js')
+    .pipe(changed('public/assets/js/main.js'))
+    .pipe(gulp.dest('public/assets/js/'))
+})
+
 // Reloading browser if views changed
 gulp.task('reload', () => {browserSync.reload()})
 
 // Serve files
 gulp.task('serve', ['pug', 'sass', 'browserSync'], () => {
-    gulp.watch('src/views/index.{jade,pug}', ['pug'])
-    gulp.watch('src/styles/styles.{scss,sass}', ['sass'])
+  gulp.watch('src/views/index.{jade,pug}', ['pug'])
+  gulp.watch('src/styles/styles.{scss,sass}', ['sass'])
+  gulp.watch('src/scripts/main.js', ['js'])
 })
